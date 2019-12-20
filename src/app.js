27e9fc555c70936784d51a3464983c16203f381e
ksdumont <ksdumont@gmail.com>
2019-12-20 10:05:26 -0800

    bookmarks-router

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const {NODE_ENV} = require('./config')
+const logger = require('./logger')
+const bookmarkRouter = require('./bookmarks/bookmarks-router')
+const uuid = require('uuid/v4')
+
 
 const app = express()
 
@@ -12,9 +16,23 @@ const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(express.json())
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path ${req.path}`);
+        return res.status(401).json({error: 'Unauthorized request'});
+    }
+    next();
+})
+
+app.use(bookmarkRouter)
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
+    res.send('How Are Ya?')
 })
 
 app.use(function errorHandler(error, req, res, next) {
---
+++ src/bookmarks/bookmarks-router.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const uuid = require('uuid/v4')
+const logger = require('../logger')
+const {bookmarks} = require('../store')
+
+const bookmarkRouter = express.Router()
+const bodyParser = express.json()
+
+bookmarkRouter.route('/bookmarks')
+.get((req, res) => {
+    res.json(bookmarks)
+}) 
+.post(bodyParser, (req, res) => {
+    const {title, url, description, rating} = req.body;
+
+    if (!title || !url || !description || !rating) {
+        logger.error(`Must include title, url, description, and rating`);
+        return res.status(400).send('Invalid data');
+    }
+    const id = uuid();
+    const bookmark = {
+        id,
+        title,
+        url,
+        description,
+        rating
+    };
+    bookmarks.push(bookmark);
+    logger.info(`Bookmark with id ${id} created`);
+    res.status(201).location(`http://localhost:8000/bookmarks/${id}`).json(bookmark);
+})
+bookmarkRouter.route('/bookmarks/:id')
+.get((req, res) => {
+    const {id} = req.params;
+    const bookmark = bookmarks.find(b => b.id === id)
+
+    if (!bookmark) {
+        logger.error(`Bookmark with id ${id} not found`)
+        return res.status(404).send('Bookmark not found')
+    }
+    res.json(bookmark);
+})
+.delete((req, res) => {
+    const {id} = req.params;
+    const bookmarkIndex = bookmarks.findIndex(b => b.id === id);
+
+    if (bookmarkIndex === -1) {
+        logger.error(`Bookmark with id ${id} not found.`)
+        return res.status(404).send('Not found')
+    }
+    bookmarks.splice(bookmarkIndex, 1)
+    logger.info(`Bookmark with id ${id} deleted.`)
+    res.status(204).end()
+})
+
+module.exports = bookmarkRouter;
